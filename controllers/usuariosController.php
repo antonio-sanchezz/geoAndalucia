cb1b44d58ede304cc047779100b1a1333d46c455
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-02-03 10:34:21 +0100

    userController:login

---
+++ controllers/usuariosController.php
@@ -0,0 +1,48 @@
+<?php
+
+function formLogin() {
+    session_start();
+    if (isset($_SESSION['usuario'])) {
+        header("Location: ?");
+    }
+
+    // Se incluye el modelo.
+    require './models/usuariosModel.php';
+    
+    // Se incluye la vista para cargar el formulario
+    include './views/loginForm.php';
+}
+
+function loginCheck() {
+
+    session_start();
+    if (isset($_SESSION['username'])) {
+        header("Location: ?");
+    }
+
+    // Se incluye el modelo.
+    require './models/usuariosModel.php';
+
+    // En $checkLogin obtenemos si el usuario y contraseña es correcto.
+    $checkLogin = login($_POST['username'], $_POST['password']);
+
+    if ($checkLogin) {
+        $_SESSION['username'] = getUser($_POST['username'])['username'];          
+
+    } else {
+        header("Location: ?controller=usuarios&action=formLogin");
+    }
+    
+}
+
+function cerrarSesion() {
+    session_start();
+
+    if (isset($_SESSION['rol'])) {
+        session_unset();
+        session_destroy();
+        header("Location: ?controller=usuarios&action=formLogin");
+    }
+}
+
+?>
