1a1cbbc2cda7dbcf194822bdaa272e8391745f9b
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-02-18 10:58:32 +0100

    dashboard css

--- models/juegoModel.php
+++ models/juegoModel.php
@@ -13,7 +13,7 @@
         try {
 
             $db = getConnection();
-            $sqlQuery = "SELECT * FROM puntuaciones WHERE username = ? LIMIT 5";
+            $sqlQuery = "SELECT * FROM puntuaciones WHERE username = ? ORDER BY puntuacion DESC LIMIT 5";
             $stmt = $db->prepare($sqlQuery);
             $stmt->bindParam(1, $username);
 
